2ba59834c36828ac4b5ea7421f462bd47dd745a5
Akhil P Nair <pakhilnair@gmail.com>
2022-06-30 15:50:20 +0530

    All funcationalities working

--- src/App.js
+++ src/App.js
@@ -1,39 +1,97 @@
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import { useState } from "react"
-import AddTask from './components/AddTask'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState, useEffect } from "react";
+import AddTask from "./components/AddTask";
 
 const App = () => {
-  const [showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false);
 
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
 
-//Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000 + 1)
-  const newTask = {id, ...task }
-  setTasks([...tasks, newTask])
-}
+    getTasks();
+  }, []);
 
-//Delete Task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task) => task.id !== id))
-}
+  //Fetching Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
 
-//Toggle reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
-}
+  //Fetching Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  //Add Task
+  const addTask = async (task) => {
+    // const id = Math.floor(Math.random() * 10000 + 1)
+    // const newTask = {id, ...task }
+    // setTasks([...tasks, newTask])
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+  };
+
+  //Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await res.json();
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)}
-      showAdd={ showAddTask }/>
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}
-      onToggle={toggleReminder} /> : 'No tasks to show '}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show "
+      )}
     </div>
-  ); 
-}
-   
+  );
+};
+
 export default App;
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,41 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const AddTask = ({ onAdd }) => {
-    const [text, setText] = useState('')
-    const [date, setDate] = useState('')
-    const [reminder, setReminder] = useState('false')
+  const [text, setText] = useState("");
+  const [date, setDate] = useState("");
+  const [reminder, setReminder] = useState("false");
 
-const onSubmit = (e) => {
-    e.preventDefault()
-    if(!text) {
-        alert('Please add text')
-        return
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("Please add text");
+      return;
     }
-    onAdd({ text, date, reminder})
-    setText('')
-    setDate('')
-    setReminder(false)
-}
+    onAdd({ text, date, reminder });
+    setText("");
+    setDate("");
+    setReminder(false);
+  };
 
   return (
-    <form className='add-form' onSubmit={onSubmit}>
-        <div className='form-control'>
-            <label>Task</label>
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)} 
-            placeholder="Add task"/>
-        </div>
-        <div className='form-control'>
-            <label>Day & Time</label>
-            <input type="text" value={date} onChange={(e) => setDate(e.target.value)}
-            placeholder="Add day & Time"/>
-        </div>
-        <div className='form-control form-control-check'>
-            <label>Set Reminder</label>
-            <input checked={reminder} type="checkbox" onChange={(e) => setReminder(e.currentTarget.checked)}/>
-        </div>
-        <input className="btn btn-block" type="submit" value='Save Task' />
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Add task"
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          placeholder="Add day & Time"
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder ? true : false}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+      <input className="btn btn-block" type="submit" value="Save Task" />
     </form>
-  )
-}
+  );
+};
 
-export default AddTask
+export default AddTask;
